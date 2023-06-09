f195181157589b355ab7c71c9535c97838325864
Zastian Pretorius <zastian00@gmail.com>
2023-06-09 09:36:30 +0100

    added weeb sit

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '9e6b7aa0'
+version = '0.1.0+dev.9e6b7aa0'
+linkified_version = '0.1.0+dev.[9e6b7aa0](https://github.com/maunium/stickerpicker/commit/9e6b7aa03640a3170a4b8330364ebb058e7d36c3)'
